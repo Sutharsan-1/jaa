29b9d7285185f16f24ed77587fe14fecf7479b46
Sutharsan-1 <36755513+Sutharsan-1@users.noreply.github.com>
2018-03-27 16:12:44 +0530

    Create integers.c

---
+++ integers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int x,y,i,sum=0;
+int c[10];
+scanf("%d",&x);
+scanf("%d",&y);
+printf("enter the array values");
+for(i=0;i<x;i++);
+{
+scanf("%d",&c[i]);
+}
+printf("sum of5d values",b);
+for(i=0;i<y;i++)
+{
+sum=sum+c[i];
+}
+printf("%d",sum);
+}
