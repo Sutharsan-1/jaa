47513cfccab60f8eeca06769403a74d71c8c885b
Sutharsan-1 <36755513+Sutharsan-1@users.noreply.github.com>
2018-03-27 15:55:55 +0530

    Create three.py

---
+++ three.py
@@ -0,0 +1,9 @@
+x=int(raw_input("enter the x value:\n"))
+y=int(raw_input("enter the y value:\n"))
+z=int(raw_input("enter the z value:\n"))
+if((x>y) and (x>z)):
+	print(x) 
+elif(z>y):
+	print(z)
+else:
+print(y)
